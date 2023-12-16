8f7736f10621d9cec1bd7cbd9fa7460b447f1d13
littlegudy <luis_rocha1999@outlook.com>
2023-12-16 19:14:38 -0300

    feat(App): structuring components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-
+import { CurrencyName } from "./components/CurrencyName";
+import { CurrencyValue } from "./components/CurrencyValue";
+import "./App.css";
+import { CurrencyAbreviation } from "./components/CurrencyAbreviation";
+import { ConvertButton } from "./components/ConvertButton";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <main>
+      <section className="background-top">
+        <CurrencyName variant="primary" name="US Dólar" />
+        <CurrencyValue variant="primary" value={271} />
+        <CurrencyAbreviation variant="primary" name="USD" />
+      </section>
+      <section>
+        <ConvertButton />
+      </section>
+      <section className="background-bottom">
+        <CurrencyAbreviation variant="secondary" name="BRL" />
+        <CurrencyValue variant="secondary" value={832} />
+        <CurrencyName variant="secondary" name="BRL Real" />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
